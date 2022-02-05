5635626929629155f70acdfa9f205a825f06ebcf
JaguarTordu17 <jaguartordu68@gmail.com>
2022-02-05 13:01:39 +0100

    rejoin function

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const mineflayer = require("mineflayer");
 
-const bot = mineflayer.createBot({
+var options = {
     host: "176.9.32.52", 
     port: 38620,
     keepAlive: true,
     checkTimeoutInterval: 15*1000,
-    username: "Server", 
-});
+    username: "Server"
+};
+
+const bot = mineflayer.createBot(options);
 
 const antiafk = require("mineflayer-antiafk");
 
@@ -16,3 +18,25 @@ bot.on("spawn", () => {
   bot.afk.setOptions({ fishing: false }); //disables fishing
   bot.afk.start();
 });
+
+bindEvents(bot);
+
+function bindEvents(bot) {
+    bot.on('login', function() {
+      console.log("I logged in.");
+      console.log("settings", bot.settings);
+    });
+
+    bot.on('kicked', function(reason) {
+      console.log("I got kicked for", reason, "lol");
+
+      bot = mineflayer.createBot(options);
+      bot.loadPlugin(antiafk);
+
+      bot.on("spawn", () => {
+        bot.afk.setOptions({ fishing: false }); //disables fishing
+        bot.afk.start();
+      });
+      bindEvents(bot);
+    });
+}
