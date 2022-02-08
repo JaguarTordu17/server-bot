55850346aa3bc0943ac40c4e7b0220e3c4b14cad
JaguarTordu17 <jaguartordu68@gmail.com>
2022-02-08 16:05:25 +0100

    undo antiafk and rejoin each 30s

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mineflayer = require("mineflayer");
-const antiafk = require("mineflayer-antiafk");
 
 var options = {
     host: "176.9.32.52", 
@@ -11,39 +10,18 @@ var options = {
 
 var bot = mineflayer.createBot(options);
 
-bot.loadPlugin(antiafk);
-bot.on("spawn", () => {
-  bot.afk.setOptions({ fishing: false }); //disables fishing
-  bot.afk.start();
-});
-
 bindEvents(bot);
 
 function bindEvents(bot) {
-
-  bot.on('error', function(err) {
-      console.log('Error attempting to reconnect: ' + err.errno + '.');
-      if (err.code == undefined) {
-          console.log('Invalid credentials OR bot needs to wait because it relogged too quickly.');
-          console.log('Will retry to connect in 30 seconds. ');
-          setTimeout(relog, 30000);
-      }
+  bot.on('login', function() {
+    console.log("I logged in.");
+    console.log("settings", bot.settings);
   });
 
-  bot.on('end', function() {
-      console.log("Bot has ended");
-      // If set less than 30s you will get an invalid credentials error, which we handle above.
-      setTimeout(relog, 30000);  
-  });
-}
+  bot.on('kicked', function(reason) {
+    console.log("I got kicked for", reason, "lol");
 
-function relog() {
-  console.log("Attempting to reconnect...");
-  bot = mineflayer.createBot(options);
-  bot.loadPlugin(antiafk);
-  bot.on("spawn", () => {
-    bot.afk.setOptions({ fishing: false }); //disables fishing
-    bot.afk.start();
+    bot = mineflayer.createBot(options);
+    bindEvents(bot);
   });
-  bindEvents(bot);
 }
